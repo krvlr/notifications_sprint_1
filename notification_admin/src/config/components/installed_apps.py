170b36d972e1529c0732205ff528e98f5c479b6d
Prok <workprok@rambler.ru>
2023-08-02 07:33:34 +0300

    Added admin panel

---
+++ notification_admin/src/config/components/installed_apps.py
@@ -0,0 +1,13 @@
+"""Application definition для модуля settings."""
+
+import os
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'notification.apps.NotificationConfig',
+]
